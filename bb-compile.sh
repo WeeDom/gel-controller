89250f3712703c9537c68294bc9cc288181349c0
Dominic Pain <dominic_pain@hotmail.com>
2026-03-27 16:38:32 +0000

    add a couple of compile handling scripts

---
+++ bb-compile.sh
@@ -0,0 +1,3 @@
+#/usr/bin/env bash
+
+arduino-cli compile   --fqbn esp32:esp32:esp32   --output-dir esp32light/breakbeam/build   esp32light/breakbeam
---
+++ bb-upload.sh
@@ -0,0 +1,1 @@
+arduino-cli upload   --fqbn esp32:esp32:esp32   --input-dir esp32light/breakbeam/build   -p /dev/ttyACM0   esp32light/breakbeam
